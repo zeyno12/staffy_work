f49e8f4611acb0fe08ba93753566b3bff860a624
zeyno12 <zeynabbagirova1@gmail.com>
2022-07-25 22:52:08 +0400

    staffy project

--- assets/js/scripnt.js
+++ assets/js/scripnt.js
@@ -9,3 +9,25 @@ $('.count').each(function () {
         }
     });
 });
+//burger
+const burger=document.querySelector(".burger");
+if(burger){
+  burger.addEventListener("click", function () {
+      const mobilnav = document.querySelector(".nav_list");
+      if (mobilnav.style.transform == "translateX(-500px)") {
+        mobilnav.style.transform = "translateX(0px)";
+      }
+      else  {
+        mobilnav.style.transform = "translateX(-500px)";
+      }
+    })
+}
+//accardion
+const openAccardion=document.querySelector(".accardion_data")
+if(openAccardion){
+  openAccardion.addEventListener("click",()=>{
+    const Opendata=document.querySelector(".accardion-form")
+    Opendata.classList.toggle("active")
+    
+  })
+}
